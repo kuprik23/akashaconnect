6e59f1024b0f8c7f8284833ecbf4f3e0825daa35
AKASHA GL <martkrupik@gmail.com>
2022-05-07 21:34:55 +0100

    commit

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -10,13 +10,13 @@ const Blog = () => (
     </div>
     <div className="gpt3__blog-container">
       <div className="gpt3__blog-container_groupA">
-        <Article imgUrl={blog01} date="Jan 21,2022" text="Akasha Metaverse officially launch." />
+        <Article imgUrl={blog01} date="Jan 21,2022" text="Akasha Metaverse officially launched." />
       </div>
       <div className="gpt3__blog-container_groupB">
-        <Article imgUrl={blog02} date="May 26, 2022" text="Fashion at akasha metaverse. a new way to express." />
-        <Article imgUrl={blog03} date="June 21, 2022" text="Elevating education with more hands-on experiment at akasha." />
-        <Article imgUrl={blog04} date="April 12, 2022" text="Collaboration and work at akasha metaverse." />
-        <Article imgUrl={blog05} date="sep 26, 2022" text="The gaming experience is a breez at akasha." />
+        <Article imgUrl={blog02} date="May 26, 2022" text="Fashion at Akasha metaverse, a new way to express." />
+        <Article imgUrl={blog03} date="June 21, 2022" text="Elevating education with more hands-on experiment at Akasha." />
+        <Article imgUrl={blog04} date="April 12, 2022" text="Collaboration and work at Akasha metaverse." />
+        <Article imgUrl={blog05} date="sep 26, 2022" text="The gaming experience is a breez at Akasha." />
       </div>
     </div>
   </div>
