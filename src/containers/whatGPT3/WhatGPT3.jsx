56552f8bfcacfe883ac4433d20ba6721d789ca9d
AKASHA GL <martkrupik@gmail.com>
2022-05-10 19:58:56 +0100

    commit

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -5,11 +5,10 @@ import './whatGPT3.css';
 const WhatGPT3 = () => (
   <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
     <div className="gpt3__whatgpt3-feature">
-      <Feature title="What is Akasha" text="Akashaverse is the pioneering  21st century digital connections. The focus is on communication, adventure and education. Built on Unreal Engine and Blockchain with our governance token $ROLAIS, we're aiming to deliver the best most secure social space for you. Join us now." />
+      <Feature title="What is Akasha" text="Akashaverse is pioneering 21st century digital connection. The focus is on communication, adventure and education. Built on Unreal Engine and Blockchain with our governance token $ROLAIS, we're aiming to deliver the best most secure social space for you. Join us now." />
     </div>
     <div className="gpt3__whatgpt3-heading">
       <h1 className="gradient__text">Virtual Heaven</h1>
-      <a target="_blank" rel="noreferrer" href="https://forms.gle/RmPfXnLzgbuYAqPY6"> <button id="bt" >Get on the WHITE LIST</button> </a>
     </div>
     <div className="gpt3__whatgpt3-container">
       <Feature title="Connection" text="Extended reality is the future of connection, the distance will vanish. Connect and share with others in ways you never dreamt off." />
