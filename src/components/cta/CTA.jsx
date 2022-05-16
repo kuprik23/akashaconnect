72dcf898aa036821b0ee32249c8ecb5009b659a6
AKASHA GL <martkrupik@gmail.com>
2022-05-17 00:17:30 +0100

    commit

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -4,7 +4,7 @@ import './cta.css';
 const CTA = () => (
   <div className="gpt3__cta">
     <div className="gpt3__cta-content">
-      <p>Join Discord</p>
+      <p>Register for Presale</p>
       <h3>Register Today & start exploring the endless possibilities.</h3>
     </div>
     <div className="gpt3__cta-btn">
