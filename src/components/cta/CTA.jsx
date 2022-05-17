0b0eabb817b00adee0511bf97ae166d1d72a5da1
AKASHA GL <martkrupik@gmail.com>
2022-05-17 07:05:43 +0100

    commit

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -8,7 +8,7 @@ const CTA = () => (
       <h3>Register Today & start exploring the endless possibilities.</h3>
     </div>
     <div className="gpt3__cta-btn">
-    <a target="_blank" rel="noreferrer" rel="noreferrer" href="https://forms.gle/RmPfXnLzgbuYAqPY6"> <button id="bt" >PRESALE</button> </a>
+    <a target="_blank" rel="noreferrer" rel="noreferrer" href="https://forms.gle/RmPfXnLzgbuYAqPY6"> <button id="bt" >The PRESALE form</button> </a>
     </div>
   </div>
 );
